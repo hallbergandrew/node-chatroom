36f585ce69f0102de9acb33573aa53877f4301b8
Andrew Hallberg <hallbergandrew@gmail.com>
2014-05-07 16:25:03 -0700

    Chat room base working, participants not being listed accurately

--- app.js
+++ app.js
@@ -4,30 +4,71 @@ var express = require("express")
             , io = require("socket.io").listen(http)
             , _ = require("underscore");
 
+/* participants format
+{
+  id: "sessionId",
+  name: "participantName"
+}
+*/
+var participants = []
+
+/* Server config */
+
 app.set("ipaddr", "127.0.0.1");
 
 app.set("port", 8080);
 
+/* Sets the views folder */
 app.set("views", __dirname + "/views");
 
+/* Sets the view engin */
 app.set("view engine", "jade");
 
 app.use(express.static("public", __dirname + "/public"));
 
 app.use(express.bodyParser());
 
+/* Server routing */
+
 app.get("/", function(request, response) {
   response.render("index");
 });
 
+//POST method to create a chat message
 app.post("/message", function(request, response) {
   var message = request.body.message;
 
   if(_.isUndefined(message) || _.isEmpty(message.trim())) {
     return response.json(400, {error: "Message is invalid"});
   }
+  var name = request.body.name;
+  //let the chat room know there was a new message
+  io.sockets.emit("incomingMessage", {message: message, name: name});
 
   response.json(200, {message: "Message received"});
+});
+
+/* Socket.IO events */
+
+//New user connects = newUser
+io.on("connection", function(socket) {
+  socket.on("newUser", function(data) {
+    participants.push({id: data.id, name: data.name});
+    io.sockets.emit("newConection", {participants: participants});
+  });
+
+//New name = nameCahnge
+  socket.on("nameChange", function(data) {
+    _.findWhere(participants, {id: socket.id}).name = data.name;
+    io.sockets.emit("nameChanged", {id: data.id, name: data.name});
+  });
+
+//Client disconnects from server = disconnect
+  socket.on("disconnect", function() {
+    participants = _.without(participants,_.findWhere(participants, {id:socket.id}));
+    io.sockets.emit("userDisconnected", {id: socket.id, sender:"system"});
+  });
+
 });
 
 http.listen(app.get("port"), app.get("ipaddr"), function() {
--- public/js/index.js
+++ public/js/index.js
@@ -12,15 +12,94 @@ function init() {
   //We'll save our session ID in a variable for later
   var sessionId = '';
 
+  //update participants list
+
+ function updateParticipants(participants) {
+   $('#participants').html('');
+   for (var i = 0; i < participants.length; i++) {
+      $('#participants').append('<span id="' + participants[i].id + '">' +
+        participants[i].name + ' ' + (participants[i].id === sessionId ? '(You)' : '') + '<br /></span>');
+    }
+  }
+
   /*
  When the client successfuly connects to the server, an
  event "connect" is emitted. Let's get the session ID and
- log it.
+ log it. And tell socket.IO theres a new user by emiting "newUser"
   */
   socket.on('connect', function () {
     sessionId = socket.socket.sessionid;
     console.log('Connected ' + sessionId);
+    socket.emit('newUser', {id: sessionId, name: $('#name').val()});
   });
+
+  //for each new connection we update the participants list
+  socket.on('newConnection', function(data) {
+    updateParticipants(data.participants);
+  });
+
+  //on userDisconnect we update span/participant display
+  socket.on('userdisconnected', function(data) {
+    $('#' + data.id).remove();
+  });
+
+  //on nameChanged update span/participant display
+  socket.on('nameChanged', function(data) {
+    $('#' + data.id).html(data.name + ' ' + (data.id === sessionId ? '(You)' : '') + '<br />');
+  });
+
+  //on incomingMessage, we prepend to the messages section
+  socket.on('incomingMessage', function(data) {
+    var message = data.message;
+    var name = data.name;
+    $('#messages').prepend('<b>' + name + '</b><br />' + message + '<hr />');
+  });
+  //if unable to connect to server, log error
+  socket.on('error', function (reason) {
+    console.log('Unable to connect to server', reason);
+  });
+
+  //sendMessage ajax POST call to server with message from textarea
+  function sendMessage() {
+    var outgoingMessage = $('#outgoingMessage').val();
+    var name = $('#name').val();
+    $.ajax({
+      url: '/message',
+      type: 'POST',
+      datatype: 'json',
+      data: {message: outgoingMessage, name: name}
+    });
+  }
+
+  //Map enter key to sendMessage if there is something in textarea
+  function outgoingMessageKeyDown(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      if ($('#outgoingMessage').val().trim().length <= 0) {
+        return;
+      }
+      sendMessage();
+      $('#outgoingMessage').val('');
+    }
+  }
+
+  //disables/enables Send button
+  function outgoingMessageKeyUp() {
+    var outgoingMessageValue = $('#outgoingMessage').val();
+    $('#send').attr('disabled', (outgoingMessageValue.trim()).length > 0 ? false : true);
+  }
+
+  //tell server when user changes name
+  function nameFocusOut() {
+    var name = $('#name').val();
+    socket.emit('nameChange', {id: sessionId, name: name});
+  }
+
+  /* Elements setup */
+  $('#outgoingMessage').on('keydown', outgoingMessageKeyDown);
+  $('#outgoingMessage').on('keyup', outgoingMessageKeyUp);
+  $('#name').on('focusout', nameFocusOut);
+  $('#send').on('click', sendMessage);
 }
 
 $(document).on('ready', init);
